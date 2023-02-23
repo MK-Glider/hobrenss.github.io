c71a1b9904ecbdbc7c7269ecdfec7fa475f3f23b
MK-Glider <114347223+MK-Glider@users.noreply.github.com>
2023-02-24 00:25:21 +0100

    ok

---
+++ app.js
@@ -0,0 +1,71 @@
+{\rtf1\ansi\deff3\adeflang1025
+{\fonttbl{\f0\froman\fprq2\fcharset0 Times New Roman;}{\f1\froman\fprq2\fcharset2 Symbol;}{\f2\fswiss\fprq2\fcharset0 Arial;}{\f3\froman\fprq2\fcharset0 Liberation Serif{\*\falt Times New Roman};}{\f4\froman\fprq2\fcharset0 Helvetica{\*\falt Arial};}{\f5\fswiss\fprq2\fcharset0 Liberation Sans{\*\falt Arial};}{\f6\fnil\fprq2\fcharset0 Microsoft YaHei;}{\f7\froman\fprq0\fcharset128 ;}{\f8\fswiss\fprq0\fcharset128 Arial;}}
+{\colortbl;\red0\green0\blue0;\red128\green128\blue128;}
+{\stylesheet{\s0\snext0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036 Standard;}
+{\s15\sbasedon0\snext16\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\sb240\sa120\keepn\ltrpar\cf0\kerning0\hich\af6\langfe2052\dbch\af5\afs28\lang1081\loch\f5\fs28\lang1036 Titre;}
+{\s16\sbasedon0\snext16\ql\sl276\slmult1{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\sb0\sa140\ltrpar\cf0\kerning0\hich\af3\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036 Corps de texte;}
+{\s17\sbasedon16\snext17\ql\sl276\slmult1{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\sb0\sa140\ltrpar\cf0\kerning0\hich\af3\langfe2052\dbch\af8\afs24\lang1081\loch\f3\fs24\lang1036 Liste;}
+{\s18\sbasedon0\snext18\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\sb120\sa120\ltrpar\cf0\i\kerning0\hich\af3\langfe2052\dbch\af8\afs24\lang1081\ai\loch\f3\fs24\lang1036 Légende;}
+{\s19\sbasedon0\snext19\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\cf0\kerning0\hich\af3\langfe255\dbch\af8\afs24\lang255\loch\f3\fs24\lang255 Index;}
+}{\info{\creatim\yr0\mo0\dy0\hr0\min0}{\revtim\yr0\mo0\dy0\hr0\min0}{\printim\yr0\mo0\dy0\hr0\min0}{\comment OpenOffice}{\vern41110}}\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720\deftab720
+
+{\*\pgdsctbl
+{\pgdsc0\pgdscuse451\pgwsxn11906\pghsxn16838\marglsxn1440\margrsxn1440\margtsxn1440\margbsxn1440\pgdscnxt0 Style de page par d\'e9faut;}}
+\formshade{\*\pgdscno0}\paperh16838\paperw11906\margl1440\margr1440\margt1440\margb1440\sectd\sbknone\sectunlocked1\pgndec\pgwsxn11906\pghsxn16838\marglsxn1440\margrsxn1440\margtsxn1440\margbsxn1440\ftnbj\ftnstart1\ftnrstcont\ftnnar\aenddoc\aftnrstcont\aftnstart1\aftnnrlc
+\pgndec\pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+window.$crisp = [];}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+window.CRISP_WEBSITE_ID = "1e7e9354-4177-4236-bbc3-ca6252174a15";}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+function loadCrispChat() \{}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+var chatContainer = document.getElementById("crisp-chat-container");}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+// create a button element for the chat widget}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+var chatButton = document.createElement("div");}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+chatButton.innerHTML = "Chat with us";}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+chatButton.setAttribute("id", "crisp-chat-button");}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+// append the button to the chat container}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+chatContainer.appendChild(chatButton);}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+// load the Crisp client script}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+var script = document.createElement("script");}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+script.src = "https://client.crisp.chat/l.js";}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+script.async = 1;}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\f4
+  }{\dbch\af4\rtlch \ltrch\loch\loch\f4
+document.getElementsByTagName("head")[0].appendChild(script);}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+\}}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+}
+\par \pard\plain \s0\ql{\*\hyphen3\hyphlead0\hyphtrail0\hyphmax255}\ltrpar\nowidctlpar\cf0\kerning1\hich\af7\langfe2052\dbch\af3\afs24\lang1081\loch\f3\fs24\lang1036\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803{\dbch\af4\rtlch \ltrch\loch\loch\f4
+loadCrispChat();}
+\par }
